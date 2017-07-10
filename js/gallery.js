e3fc3e03b270a64a0a0544b661e967b26a0b9d53
Umashankar Pardhi <u.pardhi@gmail.com>
2017-07-10 16:06:36 +0530

    added jhhitn node package and fixed code review points

--- js/gallery.js
+++ js/gallery.js
@@ -2,14 +2,14 @@
 
     $.fn.imageGallery = function(options) {
         debugger;
-        if (this.length == 0) return this;
+        if (this.length === 0) return this;
         var el = this;
         var settings = $.extend({
             imageCategory: [],
             categoryImages: [],
-			isLocalImage:true,
-			categoryTemplate:'<a href="javascript:void(0)" class="sortLink" data-category="categoryId">categoryName</>',
-			imageTemplate:'<a href="imageSrc" class="thumb-image fancybox"  title="imageCaption" rel="group" style="opacity:0"><img src="imageSrc" alt="imageCaption" /> <span>imageCaption</span></a>'
+            isLocalImage: true,
+            categoryTemplate: '<a href="javascript:void(0)" class="sortLink" data-category="categoryId">categoryName</>',
+            imageTemplate: '<a href="imageSrc" class="thumb-image fancybox"  title="imageCaption" rel="group" style="opacity:0"><img src="imageSrc" alt="imageCaption" /> <span>imageCaption</span></a>'
         }, options);
 
         /**
@@ -24,57 +24,57 @@
         var init = function() {
             renderCategoryList();
             renderCategoryImageList();
-        }
+        };
         var renderCategoryList = function() {
-			 var catList = getCategoryList();
-			if(catList && catList.length > 1){
-				
-				var catListHTML = [];
-				for (var i = 0; i < catList.length; i++) {
-					var catDetails = catList[i];
-					catListHTML.push(replaceAll(settings.categoryTemplate, {categoryId:catDetails.ImageCatId,categoryName:catDetails.ImageCatName}))
-				}
-				var div = $('<div>', {
-					'class': 'cat-wrapper filter'
-				})
-				el.append(div);
-				$(el.selector + '> div').html(catListHTML);
-				$('.cat-wrapper [data-category]').bind('click',refreshCatImages)
-				
-			}
-           
-        }
-		var renderImageUI = function(imageList){
-			$('.image-gal-img-wrapper').remove();
+            var catList = getCategoryList();
+            if (catList && catList.length > 1) {
+
+                var catListHTML = [];
+                for (var i = 0; i < catList.length; i++) {
+                    var catDetails = catList[i];
+                    catListHTML.push(replaceAll(settings.categoryTemplate, { categoryId: catDetails.ImageCatId, categoryName: catDetails.ImageCatName }));
+                }
+                var div = $('<div>', {
+                    'class': 'cat-wrapper filter'
+                });
+                el.append(div);
+                $(el.selector + '> div').html(catListHTML);
+                $('.cat-wrapper [data-category]').bind('click', refreshCatImages);
+
+            }
+
+        };
+        var renderImageUI = function(imageList) {
+            $('.image-gal-img-wrapper').remove();
             var imageNodeList = [];
             for (var i = 0; i < imageList.length; i++) {
                 var imageDetails = imageList[i];
-                imageNodeList.push(replaceAll(settings.imageTemplate, {imageSrc:imageDetails.ImageUrl,imageId:imageDetails.ImageId,imageCaption:imageDetails.ImageName}))
+                imageNodeList.push(replaceAll(settings.imageTemplate, { imageSrc: imageDetails.ImageUrl, imageId: imageDetails.ImageId, imageCaption: imageDetails.ImageName }));
             }
             var div = $('<div>', {
                 'class': 'image-gal-img-wrapper'
-            })
+            });
             el.append(div);
 
             $(el.selector + '> div.image-gal-img-wrapper').html(imageNodeList);
-			
-			animateThumbs();
-			
-		}
-		
+
+            animateThumbs();
+
+        };
+
         var getCategoryList = function() {
             return settings.imageCategory;
 
-        }
+        };
         var renderCategoryImageList = function() {
-            var defaultCateforyId = getDefaultCategory()
-            getCategoryImage(defaultCateforyId,function(categoryImage){
-				 renderImageUI(categoryImage);
-				
-			});
-           
+            var defaultCategoryId = getDefaultCategory();
+            getCategoryImage(defaultCategoryId, function(categoryImage) {
+                renderImageUI(categoryImage);
 
-        }
+            });
+
+
+        };
         var getDefaultCategory = function() {
             var listOfCategory = settings.imageCategory;
             if (listOfCategory) {
@@ -91,27 +91,27 @@
             }
             return 0;
 
-        }
-        var getCategoryImage = function(categoryId,callback) {
+        };
+        var getCategoryImage = function(categoryId, callback) {
             if (categoryId && callback) {
                 if (settings.isLocalImage) {
                     callback(getCategoryImageFromLocal(categoryId));
                 } else {
-                    getCategoryImageFromServer(categoryId,function(images){
-						callback(images)
-					});
+                    getCategoryImageFromServer(categoryId, function(images) {
+                        callback(images);
+                    });
                 }
 
             }
 
-        }
+        };
         var getCategoryImageFromLocal = function(categoryId) {
             if (categoryId) {
                 var categoryImages = settings.categoryImages;
                 if (categoryImages) {
                     for (var i = 0; i < categoryImages.length; i++) {
                         var categoryImage = categoryImages[i];
-                        if (categoryImage.categoryId == categoryId || categoryImage.CategoryId == categoryId) {
+                        if (categoryImage.categoryId === categoryId || categoryImage.CategoryId === categoryId) {
                             return categoryImage.ImageList;
 
                         }
@@ -121,54 +121,53 @@
             }
 
 
-        }
-		/* Function for getting Images from server
-		   @param {categoryId} {Number} Category Id for geeting list of images using this categoryId
-		   @param {callback} {function} Callback funtion call once image received froms server.
-		   
+        };
+        /* Function for getting Images from server
+               @param {categoryId} {Number} Category Id for getting list of images using this categoryId
+               @param {callback} {function} Callback function call once image received from server.
+               
 		
-		*/
+            */
         var getCategoryImageFromServer = function(categoryId, callback) {
             if (categoryId) {
                 $.get("demo_test.asp", function(data, status) {
-					callback(data);
+                    callback(data);
                     alert("Data: " + data + "\nStatus: " + status);
                 });
             }
 
 
-        }
-		/* Helper function for replacing perticular keys from string and use for generating Image Node and Category Node
-		   @param {str}{String} String containing the list of variables
-		   @param {mapObj} {Object} List of values that need to replace from base string 
-		   @return replaced String.
-		*/
-		var replaceAll =function(str,mapObj){
-			var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
-			return str.replace(re, function(matched){
-				return mapObj[matched];
-			});
-		}
-		/* Function use for refressing images according to the selected category. 
-		   @param {Object}{e} Event object 
-		   
-		*/
-		var refreshCatImages = function(e){
-			var categoryId= $(this).attr('data-category');	
-			getCategoryImage(categoryId,function(categoryImage){
-				  renderImageUI(categoryImage);
-			});
-          
-			
-		}
-		var animateThumbs =function () {
-			$('.thumb-image').css('display', 'inline-block').animate({
+        };
+        /* Helper function for replacing particular keys from string and use for generating Image Node and Category Node
+           @param {str}{String} String containing the list of variables
+           @param {mapObj} {Object} List of values that need to replace from base string 
+           @return replaced String.
+        */
+        var replaceAll = function(str, mapObj) {
+            var re = new RegExp(Object.keys(mapObj).join("|"), "gi");
+            return str.replace(re, function(matched) {
+                return mapObj[matched];
+            });
+        };
+        /* Function use for refresh images according to the selected category. 
+           @param {Object}{e} Event object 
+           
+        */
+        var refreshCatImages = function(e) {
+            var categoryId = $(this).attr('data-category');
+            getCategoryImage(categoryId, function(categoryImage) {
+                renderImageUI(categoryImage);
+            });
+
+
+        };
+        var animateThumbs = function() {
+            $('.thumb-image').css('display', 'inline-block').animate({
                 'opacity': 1
             }, 500);
-		}
+        };
         init();
-    }
+    };
 
 
 }(jQuery));
-
